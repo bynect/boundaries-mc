bc5a7178bd5fa39e6f10094b4cf8f2944b98a4ab
bynect <68197565+bynect@users.noreply.github.com>
2024-07-15 22:32:14 +0200

    Template

---
+++ src/main/kotlin/me/bynect/boundaries/Boundaries.kt
@@ -0,0 +1,14 @@
+package me.bynect.boundaries
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Boundaries : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
